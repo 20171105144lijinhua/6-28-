86e96696bf644c7f42424f131ca94b1c18bbd469
李锦华 <1045615337@qq.com>
2018-06-26 15:31:53 +0800

    基础

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  joj
+//
+//  Created by 李锦华 on 18/6/26.
+//  Copyright © 2018年 李锦华. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
